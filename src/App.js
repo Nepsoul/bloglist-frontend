33e4abcd8021908555736011a62e1f432c06143c
nepsoul <nirumagar901@gmail.com>
2022-11-25 16:29:36 +0545

    chore: expandation of app to allow logged-in user to add new blog

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ const App = () => {
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
 
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [url, setUrl] = useState("");
+
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
@@ -74,6 +78,61 @@ const App = () => {
     setUser(null);
   };
 
+  const handleBlogcreate = async (event) => {
+    event.preventDefault();
+    const newBlog = {
+      title,
+      author,
+      url,
+    };
+
+    const createdBlog = await blogService.create(newBlog);
+    setBlogs(blogs.concat(createdBlog));
+    setTitle("");
+    setAuthor("");
+    setUrl("");
+  };
+
+  const blogForm = () => {
+    return (
+      <form onSubmit={handleBlogcreate}>
+        <div>
+          title:{""}
+          <input
+            type="text"
+            name="title"
+            value={title}
+            onChange={(event) => {
+              setTitle(event.target.value);
+            }}
+          />
+        </div>
+        <div>
+          author:{""}
+          <input
+            type="text"
+            name="author"
+            value={author}
+            onChange={(event) => {
+              setAuthor(event.target.value);
+            }}
+          />
+        </div>
+        <div>
+          url:{""}
+          <input
+            type="text"
+            name="url"
+            value={url}
+            onChange={(event) => {
+              setUrl(event.target.value);
+            }}
+          />
+        </div>
+      </form>
+    );
+  };
+
   return (
     <div>
       <h2>blogs</h2>
@@ -87,6 +146,13 @@ const App = () => {
         <>
           <span>{user.name} logged-in </span>
           <button onClick={logOut}>log out</button>
+
+          <h2>create new</h2>
+          {blogForm()}
+
+          <button type="submit" onClick={handleBlogcreate}>
+            create
+          </button>
 
           {blogs.map((blog) => (
             <Blog key={blog.id} blog={blog} />
